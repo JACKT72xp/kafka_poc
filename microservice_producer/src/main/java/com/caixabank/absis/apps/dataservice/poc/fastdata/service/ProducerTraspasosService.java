657749b89289f6a7c6723d9be67a94f8edc29ec1
MariaSG <201974609+Maria-SG1@users.noreply.github.com>
2025-05-16 08:47:14 +0200

    Añadidos los scripts de oracle

--- microservice_producer/src/main/java/com/caixabank/absis/apps/dataservice/poc/fastdata/service/ProducerTraspasosService.java
+++ microservice_producer/src/main/java/com/caixabank/absis/apps/dataservice/poc/fastdata/service/ProducerTraspasosService.java
@@ -4,9 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
-
-
-
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -42,11 +39,12 @@ public class ProducerTraspasosService implements CommandLineRunner{
 
 	private void producirMensajes(){
 		while (true) {
-			System.out.println("Producing messages...");
+			//System.out.println("Producing messages...");
 			try {
 				Random random = new Random();
 				int randomInt = random.nextInt(100);
 				String message = "Hello World: " + randomInt;
+				System.out.println(message);
 				send(message);
 				Thread.sleep(3000);
 			} catch (InterruptedException e) {
