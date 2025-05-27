e4ef4393de0a2acc1c14b2fbdc6172e987951124
U01ABDD9 <U01ABDD9@lacaixa.es>
2025-05-27 09:55:14 +0200

    BBDD Ejecutando scripts BBDD oracle solo la primera vez

--- containersconfig/scripts/oracle/init-oracle.sh
+++ containersconfig/scripts/oracle/init-oracle.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
 # Oracle DB 
-echo "Esperando Oracle "
+echo "Esperando Oracle..."
 sleep 5
 while ! echo exit | sqlplus system/$ORACLE_PASSWORD@//localhost:1521/XEPDB1 > /dev/null 2>&1; do
     echo "Esperando que el Listener esté listo..."
     sleep 5
 done
 
-echo "Creando nuevo usuario..."
-sqlplus system/$ORACLE_PASSWORD@//localhost:1521/XEPDB1 <<EOF
+# Comprobar si el usuario ya existe
+USER_EXISTS=$(sqlplus -s system/$ORACLE_PASSWORD@//localhost:1521/XEPDB1 <<EOF
+SET HEADING OFF
+SET FEEDBACK OFF
+SELECT username FROM dba_users WHERE username = 'APPUSER';
+EXIT;
+EOF
+)
+
+if [[ -z "$USER_EXISTS" ]]; then
+    echo "Usuario APPUSER no existe. Creándolo..."
+
+    sqlplus system/$ORACLE_PASSWORD@//localhost:1521/XEPDB1 <<EOF
 CREATE USER appuser IDENTIFIED BY appuser123;
 GRANT CONNECT, RESOURCE TO appuser;
 GRANT CREATE TABLE, CREATE VIEW, CREATE PROCEDURE TO appuser;
@@ -18,12 +29,11 @@ GRANT UNLIMITED TABLESPACE TO appuser;
 EXIT;
 EOF
 
-#  DDL script
-echo "Ejecutando init.sql script..."
-###sqlplus system/$ORACLE_PASSWORD@//localhost:1521/XEPDB1 @/oracle/init-oracle.sql
+    echo "Ejecutando init.sql script..."
+    sqlplus appuser/appuser123@//localhost:1521/XEPDB1 @/oracle/init-oracle.sql
+else
+    echo "El usuario APPUSER ya existe. Saltando creación e inicialización."
+fi
 
-sqlplus appuser/appuser123@//localhost:1521/XEPDB1 @/oracle/init-oracle.sql
-
-# 
+# Mantener el contenedor en ejecución
 tail -f /dev/null
-
