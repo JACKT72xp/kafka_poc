c7c0fcd727c1f50308df1597dd7ec80d0feda8f9
MariaSG <201974609+Maria-SG1@users.noreply.github.com>
2025-05-28 09:19:02 +0200

    Modificado script init-db2.sh comprobar si las tablas existen

--- containersconfig/scripts/db2/db2_init_container/init-db2.sh
+++ containersconfig/scripts/db2/db2_init_container/init-db2.sh
@@ -1,21 +1,22 @@
+
 #!/bin/bash
- 
 
+check_tables_exist() {
+    local tables=("EXPEDIENTES" "ORDENES" "FONDOS" "TRASPASOS" "RETENCIONES" "CONTRATOS" "BORRADOS") 
+    for table in "${tables[@]}"; do
+        if ! su - db2inst1 -c "db2 connect to $DBNAME && db2 -x \"SELECT TABNAME FROM SYSCAT.TABLES WHERE TABNAME = '$table'\"" | grep -q "$table"; then
+            return 1  
+        fi
+    done
+    return 0  
+}
 
-check_schema_exists() {
-su - db2inst1 -c "db2 connect to TESTDB && db2 -x \"SELECT SCHEMANAME FROM SYSCAT.SCHEMATA WHERE SCHEMANAME = 'DEMO'\"" | grep DEMO
-}
- 
-check_table_exists() {
-su - db2inst1 -c "db2 connect to TESTDB && db2 -x \"SELECT TABNAME FROM SYSCAT.TABLES WHERE TABSCHEMA = 'DEMO' AND TABNAME = 'CUSTOMERS'\"" | grep CUSTOMERS
-}
- 
-# Ejecutar script de inicialización si el esquema o la tabla no existen
-if ! check_schema_exists || ! check_table_exists; then
+if ! check_tables_exist; then
     echo "🚀 Ejecutando script de inicialización..."
-    su - db2inst1 -c "db2 connect to TESTDB && db2 -tvf /init-db2/init-db2.sql"
+    su - db2inst1 -c "db2 connect to $DBNAME && db2 -tvf /init-db2/init-db2.sql"
 else
-    echo "El esquema DEMO y la tabla CUSTOMERS ya existen. No se requiere inicialización."
+    echo "Todas las tablas requeridas ya existen. No se requiere inicialización."
 fi
- 
+
+
 chown root:db2iadm1 /database/config/db2inst1/sqllib/adm/fencedid
